248f49ecb4acbc76acaf440b8d1321a573393123
Wasi-M7 <abdulwasi.mohammad@jpschools.org>
2024-06-13 16:17:24 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,7 +33,7 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for (var i = 0; i < 1000; i++) {
+        for (var i = 0; i < 100; i++) {
             makeCircle();
           }
 
