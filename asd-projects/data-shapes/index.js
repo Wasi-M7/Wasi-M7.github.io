ae1ac98eca811ce1013b9c48c7411906de209464
Wasi-M7 <abdulwasi.mohammad@jpschools.org>
2025-06-09 16:15:59 +0000

    saving data shapes progress

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -68,7 +68,7 @@ $(document).ready(function () {
   }
   // TODO 5-a: add a function that handles the bad display type
   function handleBad (data, repeat) {
-    repeat++
+    repeat = repeat + 1
     setBackgroundWithSimple(data, repeat)
     animationDetails.displayType = 3
   }
