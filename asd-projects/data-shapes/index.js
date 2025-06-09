e287b767d3f3988b6eb1929dcfd04bf351d33a7c
Wasi-M7 <abdulwasi.mohammad@jpschools.org>
2025-06-09 13:11:05 +0000

    saving data shapes progress

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
   // TODO 0: complete
 
   // TODO 2: add a new property to all data shapes
-  for (var i = 0; i <= dataShapes.length; i++) {
+  for (var i = 0; i <= dataShapes; i++) {
     var currentShape = dataShapes[i];
     if (currentShape.color === "red") {
       goodBehavior = "bounce";
