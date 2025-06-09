8c8bc2fdd8756971e823b20b28ecfc434e922faa
Wasi-M7 <abdulwasi.mohammad@jpschools.org>
2025-06-09 16:08:54 +0000

    saving data shapes progress

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -62,7 +62,10 @@ $(document).ready(function () {
     animationDetails.displayType = 1
   }
   // TODO 4-a: add a function that handles the good display type
-
+  function handleGood (color, shape, repeat){
+    setBackgroundWithSimple(color, shape, repeat)
+    animationDetails.displayType = 2
+  }
   // TODO 5-a: add a function that handles the bad display type
 
   /////////////////////////////////////////////////
@@ -76,6 +79,8 @@ $(document).ready(function () {
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
+    var currentShape = dataShapes[currentIndex]
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat)
   }
 
   function badDisplay() {
